67c30059a815e58bf0153ba076263db516b32550
neuralassembly <neuralassembly@gmail.com>
2013-12-26 13:22:30 +0900

    Adding the Socket timeout

--- src/com/camera/simplemjpeg/MjpegActivity.java
+++ src/com/camera/simplemjpeg/MjpegActivity.java
@@ -2,12 +2,14 @@ package com.camera.simplemjpeg;
 
 import java.io.IOException;
 import java.net.URI;
+
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
+
 import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -187,26 +189,31 @@ public class MjpegActivity extends Activity {
     public class DoRead extends AsyncTask<String, Void, MjpegInputStream> {
         protected MjpegInputStream doInBackground(String... url) {
             //TODO: if camera has authentication deal with it and don't just not work
-            HttpResponse res = null;
+            HttpResponse res = null;         
             DefaultHttpClient httpclient = new DefaultHttpClient(); 
             HttpParams httpParams = httpclient.getParams();
             HttpConnectionParams.setConnectionTimeout(httpParams, 5*1000);
-            Log.d(TAG, "1. Sending http request");
+            HttpConnectionParams.setSoTimeout(httpParams, 5*1000);
+            if(DEBUG) Log.d(TAG, "1. Sending http request");
             try {
                 res = httpclient.execute(new HttpGet(URI.create(url[0])));
-                Log.d(TAG, "2. Request finished, status = " + res.getStatusLine().getStatusCode());
+                if(DEBUG) Log.d(TAG, "2. Request finished, status = " + res.getStatusLine().getStatusCode());
                 if(res.getStatusLine().getStatusCode()==401){
                     //You must turn off camera User Access Control before this will work
                     return null;
                 }
                 return new MjpegInputStream(res.getEntity().getContent());  
             } catch (ClientProtocolException e) {
-                e.printStackTrace();
-                Log.d(TAG, "Request failed-ClientProtocolException", e);
+            	if(DEBUG){
+	                e.printStackTrace();
+	                Log.d(TAG, "Request failed-ClientProtocolException", e);
+            	}
                 //Error connecting to camera
             } catch (IOException e) {
-                e.printStackTrace();
-                Log.d(TAG, "Request failed-IOException", e);
+            	if(DEBUG){
+	                e.printStackTrace();
+	                Log.d(TAG, "Request failed-IOException", e);
+            	}
                 //Error connecting to camera
             }
             return null;
@@ -222,7 +229,7 @@ public class MjpegActivity extends Activity {
     
     public class RestartApp extends AsyncTask<Void, Void, Void> {
         protected Void doInBackground(Void... v) {
-			MjpegActivity.this.finish();
+        	MjpegActivity.this.finish();
             return null;
         }
 
